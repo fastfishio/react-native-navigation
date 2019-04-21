e561c697b3a43ba775b4e66fb5c36afd39332e7c
Amr Labib <amaly@noon.com>
2019-04-21 11:32:05 +0400

    selectedTabIndex and unselectedTabIndex minor code enhancement

--- ios/RCCTabBarController.m
+++ ios/RCCTabBarController.m
@@ -27,11 +27,12 @@
         [[[RCCManager sharedInstance].getBridge uiManager] configureNextLayoutAnimation:nil withCallback:^(NSArray* arr){} errorCallback:^(NSArray* arr){}];
     });
     
-    if (tabBarController.selectedIndex != [tabBarController.viewControllers indexOfObject:viewController]) {
-        NSDictionary *body = @{
+    NSDictionary *body = @{
                                @"selectedTabIndex": @([tabBarController.viewControllers indexOfObject:viewController]),
                                @"unselectedTabIndex": @(tabBarController.selectedIndex)
                                };
+
+    if (tabBarController.selectedIndex != [tabBarController.viewControllers indexOfObject:viewController]) {
         [RCCTabBarController sendScreenTabChangedEvent:viewController body:body];
         
         [[[RCCManager sharedInstance] getBridge].eventDispatcher sendAppEventWithName:@"bottomTabSelected" body:body];
@@ -47,11 +48,6 @@
         }
         return YES;
     } else {
-        NSDictionary *body = @{
-                               @"selectedTabIndex": @([tabBarController.viewControllers indexOfObject:viewController]),
-                               @"unselectedTabIndex": @(tabBarController.selectedIndex)
-                               };
-
         [RCCTabBarController sendScreenTabPressedEvent:viewController body:body];
         return NO;
     }
